9628e77c226847621f1eda15168698bb62c568c7
Satyam <satyamkumarguptaraj@gmail.com>
2024-09-03 22:19:01 +0530

    Contact section is completed

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Projects from "@/sections/Projects";
 import Tape from "@/sections/Tape";
 import Testimonials from "@/sections/Testimonials";
 import About from "@/sections/About";
+import Contact from "@/sections/Contact";
 export default function Home() {
   return (
     <div className="">
@@ -15,6 +16,7 @@ export default function Home() {
       <Tape/>
       <Testimonials/>
       <About/>
+      <Contact/>
     </div>
   );
 }
--- sections/Contact.jsx
+++ sections/Contact.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import { Card } from "@/components/Card";
+import { ArrowUpRightIcon } from "@/components/SvgIcons";
 
 export default function Contact() {
-  return (
-    <div>Contact</div>
-  )
+	return (
+		<div className="py-16 pt-14 lg:py-24 lg:pt-20 px-4 flex justify-center">
+			<div className="container">
+				<div className=" ">
+					<Card classname="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 rounded-3xl text-center md:text-left">
+						<div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
+							<div className="flex flex-col">
+								<h2 className="font-serif text-2xl md:text-3xl">
+									Let's Create something amazing together
+								</h2>
+								<p className="text-sm mt-2 md:text-base">
+									Ready to bring your next project to life? Let's connect and
+									discuss how I can help you achieve your goals.
+								</p>
+							</div>
+							<div>
+								<button className="inline-flex items-center text-white bg-gray-900 px-6 h-12 rounded-xl gap-2 w-max">
+									<span className="font-semibold">Contact Me</span>
+									<div className="">
+										<ArrowUpRightIcon />
+									</div>
+								</button>
+							</div>
+						</div>
+					</Card>
+				</div>
+			</div>
+		</div>
+	);
 }
